eb6246f9bdf5a493be91874fc9b6058a1ce35c13
ฺBank_Thanomsup <titikorn04629@hotmail.com>
2023-09-04 01:47:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,23 +10,23 @@ const morgan = require("morgan");
 const productRouter = express.Router;
 
 app.use(morgan("combined"));
-
 app.use(express.static(path.join(__dirname, "/public/")));
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
     res.send("Hello World !! I'm Product")
 });
 
-productRouter.route("/").get((req, res) => {
+productRouter.route("/1").get((req, res) => {
     res.send("Hello World !! I'm Product 1")
 });
 
 app.use("/products", productRouter);
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    username: "Bankza55+",
-    customers: ["kitti", "Kittikon", "Kitty"],
+  res.render("index", {username: "Bankza55+",customers: ["kitti", "Kittikon", "Kitty"],
   });
 });
 
@@ -34,5 +34,4 @@ app.listen(PORT, () => {
   debug("Listening on port" + chalk.red(" : " + PORT));
 });
 
-app.set("views", "./src/views");
-app.set("view engine", "ejs");
+
