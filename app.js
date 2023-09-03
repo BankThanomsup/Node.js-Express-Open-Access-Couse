5b93da121eebc906ff63623aabfc620205202086
ฺBank_Thanomsup <titikorn04629@hotmail.com>
2023-09-04 01:04:22 +0700

    update code rount 2

--- app.js
+++ app.js
@@ -2,17 +2,25 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 const path = require('path');
+const debug = require('debug')('app');
 const chalk = require("chalk");
+const morgan = require('morgan');
+
+
+app.use(morgan('combined'));
 
 app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/",(req,res) =>{
 
-    res.send('hello thitikorn thanomsup');
+    res.render('index',{username:'Bankza55+', customers:["kitti","Kittikon","Kitty"]});
 })
 
-app.listen(port, ()=>{
-    console.log("Listening on port",chalk.red(port));
+app.listen(PORT, ()=>{
+   debug("Listening on port" + chalk.red(" : " + PORT));
 })
+
+app.set("views","./src/views");
+app.set("view engine" ,"ejs")
