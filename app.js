7aec6f5c5a9765c03fcdf4f4baf405d3271674c9
ฺBank_Thanomsup <titikorn04629@hotmail.com>
2023-09-04 11:27:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-    res.send("Hello World !! I'm Product")
-});
+// productRouter.route("/").get((req, res) => {
+//     res.send("Hello World !! I'm Product")
+// });
 
-productRouter.route("/1").get((req, res) => {
-    res.send("Hello World !! I'm Product 1")
-});
+// productRouter.route("/1").get((req, res) => {
+//     res.send("Hello World !! I'm Product 1")
+// });
 
-app.use("/products", productRouter);
+// app.use("/products", productRouter);
 
 app.get("/", (req, res) => {
   res.render("index", {username: "Bankza55+",customers: ["kitti", "Kittikon", "Kitty"],
