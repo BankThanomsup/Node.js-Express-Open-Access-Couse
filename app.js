120463c45dc14e8872c9ec264a22437e858d9794
ฺBank_Thanomsup <titikorn04629@hotmail.com>
2023-09-04 11:41:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require("path");
 const debug = require("debug")("app");
 const chalk = require("chalk");
 const morgan = require("morgan");
-const productRouter = express.Router;
+const productRouter = express.Router();
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -15,23 +15,23 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-// productRouter.route("/").get((req, res) => {
-//     res.send("Hello World !! I'm Product")
-// });
+productRouter.route("/").get((req, res) => {
+  res.send("Hello World !! I'm Product");
+});
 
-// productRouter.route("/1").get((req, res) => {
-//     res.send("Hello World !! I'm Product 1")
-// });
+productRouter.route("/1").get((req, res) => {
+  res.send("Hello World !! I'm Product 1");
+});
 
 app.use("/Products", productRouter);
 
 app.get("/", (req, res) => {
-  res.render("index", {username: "Bankza55+",customers: ["kitti", "Kittikon", "Kitty"],
+  res.render("index", {
+    username: "Bankza55+",
+    customers: ["kitti", "Kittikon", "Kitty"],
   });
 });
 
 app.listen(PORT, () => {
   debug("Listening on port" + chalk.red(" : " + PORT));
 });
-
-
