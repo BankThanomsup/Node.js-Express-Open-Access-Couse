3c1f6f3fce138ea8a966f21831ae196f1c5afa30
ฺBank_Thanomsup <titikorn04629@hotmail.com>
2023-09-04 14:26:53 +0700

    Update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //Import module
 
-const express = require("express");
+
 const app = express();
 const PORT = process.env.PORT;
 const path = require("path");
-const products = require("./data/products.json");
+
 const debug = require("debug")("app");
 const chalk = require("chalk");
 const morgan = require("morgan");
-const productRouter = express.Router();
+const productRouter = require("./src/router/productsRouter")
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -16,16 +16,7 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-  res.render("products", {products});
-});
-  
-productRouter.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  res.render("product",{
-    product:products[id],
-  })
-});
+
 
 app.use("/products", productRouter);
 
